06967a347d552272d9d0a4b600434f9e78ab0d35
airbob,gu <bobair31@gmail.com>
2016-05-16 02:58:15 +0800

    php v1.4

--- admin_login.php
+++ admin_login.php
@@ -8,13 +8,15 @@
 require_once ("mysql_connect.php");
 $email=$_POST['email'];
 $password=$_POST['password'];
+echo $email $password;
 $sql="selct * from admin where email='$email'";
 $result=mysqli_query($con,$sql);
 $rs=mysqli_fetch_array($result);
+echo $rs['password'];
 if($password=$rs['password']){
 //    return true;
-    echo "<script type='text/javascript'>alert('登陆成功');location='./admin-form.html';</script>";
+    echo "<script type='text/javascript'>alert('登陆成功');location='./admin/admin-form.html';</script>";
 }else{
 //    return false;
-    echo "<script type='text/javascript'>alert('密码错误');location='./index.html';</script>";
+    echo "<script type='text/javascript'>alert('密码错误');location='./admin/index.html';</script>";
 }
