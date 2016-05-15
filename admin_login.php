9b836bc8da8d106fad3f0f1b7e9542b845f5fcc5
airbob,gu <bobair31@gmail.com>
2016-05-16 02:47:06 +0800

    php v1.1

--- admin_login.php
+++ admin_login.php
@@ -5,3 +5,16 @@
  * Date: 16/5/16
  * Time: 01:20
  */
+require_once ("mysql_connect.php");
+$email=$_POST['email'];
+$password=$_POST['password'];
+$sql="selct * from admin where email='$email'";
+$result=mysqli_query($con,$sql);
+$rs=mysqli_fetch_array($result);
+if($password=$rs['password']){
+//    return true;
+    echo "<script type='text/javascript'>alert('登陆成功');location='admin.php';</script>";
+}else{
+//    return false;
+    echo "<script type='text/javascript'>alert('密码错误');location='index.html';</script>";
+}
--- post.php
+++ post.php
@@ -5,3 +5,12 @@
  * Date: 16/5/16
  * Time: 00:50
  */
+require_once ("mysql_connect.php");
+$title=$_POST['post_title'];
+$ab=$_POST['post_ab'];
+$content=$_POST['content'];
+$sql="insert into post(title, ab, content) VALUES ('$title','$ab','$content')";
+$result =mysqli_query($con,$sql);
+if(!$result){
+    echo "<script type='text/javascript'>alert('发布成功');location='./admin/admin-form.html';</script>";
+}
